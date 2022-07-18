097ad90a34704ee8411eec70dec9cceac5c7ced6
20018859 <20018859@f>
2022-07-18 12:45:34 +0800

    Test Controller + Test.html

---
+++ C300-DeFiners/src/main/java/c300/definers/fyp/TestController.java
@@ -0,0 +1,14 @@
+package c300.definers.fyp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+	@GetMapping("/test")
+	public String demo() {
+		return "test";
+	}
+
+}
