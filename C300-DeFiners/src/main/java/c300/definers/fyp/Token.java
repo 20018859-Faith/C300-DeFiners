997a9e429cdbdc3b47846d8a6a1d612196d7fd14
20018859 <20018859@f>
2022-07-30 20:20:26 +0800

    Update pool codes

--- C300-DeFiners/src/main/java/c300/definers/fyp/Token.java
+++ C300-DeFiners/src/main/java/c300/definers/fyp/Token.java
@@ -1,9 +1,14 @@
 package c300.definers.fyp;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -15,20 +20,23 @@ public class Token {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	
+
 	@NotNull
 	@NotEmpty(message = "Contract address cannot be empty!")
 	private String address;
-	
+
 	@NotNull
 	@NotEmpty(message = "Token name cannot be empty!")
 	@Size(min = 2, max = 100, message = "Token name length must be between 2 and 100 charaters")
 	private String name;
-	
+
 	@NotNull
 	private double deci;
-	
+
 	private String imgToken;
+
+//	@OneToMany(mappedBy = "token")
+//	private Set<Pool> Pool;
 
 	public int getId() {
 		return id;
@@ -69,5 +77,5 @@ public class Token {
 	public void setImgToken(String imgToken) {
 		this.imgToken = imgToken;
 	}
-	
+
 }
