6e1ed9f61700ac9a9cdf902ab60890c459aff2d5
20018859 <20018859@f>
2022-07-30 20:26:17 +0800

    Update token.java

--- C300-DeFiners/src/main/java/c300/definers/fyp/Token.java
+++ C300-DeFiners/src/main/java/c300/definers/fyp/Token.java
@@ -1,10 +1,6 @@
 package c300.definers.fyp;
 
-<<<<<<< HEAD
-
-=======
 import java.util.Set;
->>>>>>> branch 'master' of https://github.com/20018859-Faith/C300-DeFiners.git
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -21,25 +17,6 @@ import javax.validation.constraints.Size;
 @Entity
 public class Token {
 
-<<<<<<< HEAD
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private int id;
-  
-  @NotNull
-  @NotEmpty(message = "Contract address cannot be empty!")
-  private String address;
-  
-  @NotNull
-  @NotEmpty(message = "Token name cannot be empty!")
-  @Size(min = 2, max = 100, message = "Token name length must be between 2 and 100 charaters")
-  private String name;
-  
-  @NotNull
-  private double deci;
-  
-  private String imgToken;
-=======
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
@@ -57,63 +34,48 @@ public class Token {
 	private double deci;
 
 	private String imgToken;
->>>>>>> branch 'master' of https://github.com/20018859-Faith/C300-DeFiners.git
 
-<<<<<<< HEAD
-  public int getId() {
-    return id;
-  }
-=======
 //	@OneToMany(mappedBy = "token")
 //	private Set<Pool> Pool;
 
 	public int getId() {
 		return id;
 	}
->>>>>>> branch 'master' of https://github.com/20018859-Faith/C300-DeFiners.git
 
-  public void setId(int id) {
-    this.id = id;
-  }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-  public String getAddress() {
-    return address;
-  }
+	public String getAddress() {
+		return address;
+	}
 
-  public void setAddress(String address) {
-    this.address = address;
-  }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-  public String getName() {
-    return name;
-  }
+	public String getName() {
+		return name;
+	}
 
-  public void setName(String name) {
-    this.name = name;
-  }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-  public double getDeci() {
-    return deci;
-  }
+	public double getDeci() {
+		return deci;
+	}
 
-  public void setDeci(double deci) {
-    this.deci = deci;
-  }
+	public void setDeci(double deci) {
+		this.deci = deci;
+	}
 
-  public String getImgToken() {
-    return imgToken;
-  }
+	public String getImgToken() {
+		return imgToken;
+	}
 
-<<<<<<< HEAD
-  public void setImgToken(String imgToken) {
-    this.imgToken = imgToken;
-  }
-  
-}
-=======
 	public void setImgToken(String imgToken) {
 		this.imgToken = imgToken;
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/20018859-Faith/C300-DeFiners.git
