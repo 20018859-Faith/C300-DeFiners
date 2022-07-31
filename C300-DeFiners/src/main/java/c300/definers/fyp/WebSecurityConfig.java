0a0b6bbcce279cacd89b3ba7a487a818b7e6bd81
20018859 <20018859@f>
2022-07-31 21:41:42 +0800

    update code

--- C300-DeFiners/src/main/java/c300/definers/fyp/WebSecurityConfig.java
+++ C300-DeFiners/src/main/java/c300/definers/fyp/WebSecurityConfig.java
@@ -12,55 +12,57 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Bean
 	public MemberDetailsService memberDetailsService() {
-	return new MemberDetailsService();
+		return new MemberDetailsService();
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
 		authProvider.setUserDetailsService(memberDetailsService());
 		authProvider.setPasswordEncoder(passwordEncoder());
-		
+
 		return authProvider;
 	}
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider (authenticationProvider());
+		auth.authenticationProvider(authenticationProvider());
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-		
-		//Members functions
-		.antMatchers("/members","/members/edit/*","/members/delete/*").hasRole("ADMIN")
-		//"/members/add" & "/members/save" open for visitors to sign up
-		
-		//Pool
-		.antMatchers("/pool/newPool").hasRole("ADMIN")
-	
-		
-		.antMatchers("/", "/members/add", "/pool", "/charts", "/about", "/docs", "/docs/definers").permitAll()
-		.antMatchers("/bootstrap/*/*").permitAll()
-		.antMatchers("/images/*").permitAll()
-		.antMatchers("/css/*").permitAll()
-		.anyRequest().authenticated()
-		.and()
-		.formLogin().loginPage("/login").permitAll()
-		.defaultSuccessUrl("/", true)
-		.and()
-		.logout().logoutUrl("/logout").permitAll()
-		.and()
-		.exceptionHandling().accessDeniedPage("/403");
+
+				// Members functions
+				.antMatchers("/members", "/members/edit/*", "/members/delete/*").hasRole("ADMIN")
+				// "/members/add" & "/members/save" open for visitors to sign up
+				.antMatchers("/members/add", "/members/save").permitAll()
+
+				// Pool
+				.antMatchers("/pool/newPool", "/pool/save", "/pool/manage", "/pool/edit/*", "/pool/edit/pool/edit/*",
+						"/pool/delete/*")
+				.hasRole("ADMIN").antMatchers("/pool").permitAll()
+
+				// Token
+				.antMatchers("/tokens/add", "/tokens/save", "/token/edit/*", "/token/delete/*")
+				.hasAnyRole("ADMIN", "USER").antMatchers("/tokens").permitAll()
+
+				// Swap
+				.antMatchers("/swap/save").hasAnyRole("ADMIN", "USER")
+
+				.antMatchers("/", "/charts", "/about", "/docs", "/docs/definers").permitAll()
+				.antMatchers("/bootstrap/*/*").permitAll().antMatchers("/images/*").permitAll().antMatchers("/css/*")
+				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll()
+				.defaultSuccessUrl("/", true).and().logout().logoutUrl("/logout").permitAll().and().exceptionHandling()
+				.accessDeniedPage("/403");
 
 	}
 }
